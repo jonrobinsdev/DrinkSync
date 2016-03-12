4d4261286aa9a9306b18ed7532d5ade3f23faee6
Jonathan Robins <jonrobns@gmail.com>
2016-03-11 23:08:32 -0500

    drawing graphics

--- DrinkSync/DrinkProgressView.swift
+++ DrinkSync/DrinkProgressView.swift
@@ -16,14 +16,64 @@ class DrinkProgressView: UIView {
     }
     
     override func drawRect(rect: CGRect) {
-        let ctx: CGContextRef = UIGraphicsGetCurrentContext()!
-        let roundRect:CGPathRef = UIBezierPath(roundedRect: CGRectMake(rect.size.width/2, rect.size.height/2, 100, 250), byRoundingCorners:.AllCorners, cornerRadii: CGSizeMake(16, 16)).CGPath
+        UIColor.grayColor().setStroke()
         
-        CGContextAddPath(ctx, roundRect)
-        CGContextSetFillColorWithColor(ctx, UIColor.redColor().CGColor)
+        //sides
+        let path = UIBezierPath()
+        path.lineWidth = 2
+        path.moveToPoint(CGPoint(
+            x:bounds.width/2 - 100,
+            y:bounds.height/6))
+        path.addLineToPoint(CGPoint(
+            x:bounds.width/2 - 100,
+            y:bounds.height/1.5))
+        path.moveToPoint(CGPoint(
+            x:bounds.width/2 + 100,
+            y:bounds.height/6))
+        path.addLineToPoint(CGPoint(
+            x:bounds.width/2 + 100,
+            y:bounds.height/1.5))
+        //bowl
+        let center = CGPoint(x:bounds.width/2, y: (bounds.height/1.5))
+        let radius: CGFloat = bounds.width - (bounds.width/2 - 100) - (bounds.width/2 - 100) + 2
+        let startAngle: CGFloat = 0
+        let endAngle: CGFloat = 3.14
+        let path2 = UIBezierPath(arcCenter: center,
+            radius: radius/2 - 2.0/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        path2.lineWidth = 2
+        path.appendPath(path2)
+        path.stroke()
         
-        CGContextClosePath(ctx)
-        CGContextFillPath(ctx)
-
+        //water
+        UIColor.blueColor().setFill()
+        
+        let waterRadius: CGFloat = bounds.width - (bounds.width/2 - 100) - (bounds.width/2 - 100) - 5
+        let path3 = UIBezierPath(arcCenter: center,
+            radius: waterRadius/2 - 2.0/2,
+            startAngle: startAngle,
+            endAngle: endAngle,
+            clockwise: true)
+        path3.lineWidth = 2
+        path3.fill()
+        
+        let path4 = UIBezierPath()
+        path4.lineWidth = 2
+        path4.moveToPoint(CGPoint(
+            x:bounds.width/2 - 96.5,
+            y:bounds.height/6))
+        path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 - 96.5,
+            y:bounds.height/1.5 + 2))
+        path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 + 96.5,
+            y:bounds.height/1.5 + 2))
+        path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 + 96.5,
+            y:bounds.height/6))
+        path4.closePath()
+        path4.fill()
     }
 }
