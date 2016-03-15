43e2c6eefde7565fb106448a5e717615d4dbe1e0
Jonathan Robins <jonrobns@gmail.com>
2016-03-15 00:16:06 -0400

    water fill logic

--- DrinkSync/DrinkProgressView.swift
+++ DrinkSync/DrinkProgressView.swift
@@ -9,8 +9,9 @@
 import UIKit
 
 class DrinkProgressView: UIView {
-    let lineWidth: CGFloat = 3
-
+    private let lineWidth: CGFloat = 2
+    private let pi = M_PI
+    let currentPercentage: CGFloat = 0.15
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
@@ -20,60 +21,100 @@ class DrinkProgressView: UIView {
         
         //sides
         let path = UIBezierPath()
-        path.lineWidth = 2
+        path.lineWidth = lineWidth
+        
         path.moveToPoint(CGPoint(
-            x:bounds.width/2 - 100,
-            y:bounds.height/6))
+            x:bounds.width/2 - bounds.width/3.5,
+            y:bounds.height/6
+        ))
         path.addLineToPoint(CGPoint(
-            x:bounds.width/2 - 100,
-            y:bounds.height/1.5))
+            x:bounds.width/2 - bounds.width/3.5,
+            y:bounds.height/1.5
+        ))
         path.moveToPoint(CGPoint(
-            x:bounds.width/2 + 100,
-            y:bounds.height/6))
+            x:bounds.width/2 + bounds.width/3.5,
+            y:bounds.height/6
+        ))
         path.addLineToPoint(CGPoint(
-            x:bounds.width/2 + 100,
-            y:bounds.height/1.5))
+            x:bounds.width/2 + bounds.width/3.5,
+            y:bounds.height/1.5
+        ))
         //bowl
         let center = CGPoint(x:bounds.width/2, y: (bounds.height/1.5))
-        let radius: CGFloat = bounds.width - (bounds.width/2 - 100) - (bounds.width/2 - 100) + 2
-        let startAngle: CGFloat = 0
-        let endAngle: CGFloat = 3.14
+        let radius: CGFloat = bounds.width - (bounds.width/2 - (bounds.width/3.5)) - (bounds.width/2 - (bounds.width/3.5)) + 1.5
+        let startAngle: CGFloat = 0.0
+        let endAngle: CGFloat = CGFloat(pi)
         let path2 = UIBezierPath(arcCenter: center,
             radius: radius/2 - 2.0/2,
             startAngle: startAngle,
             endAngle: endAngle,
             clockwise: true)
-        path2.lineWidth = 2
+        path2.lineWidth = lineWidth
         path.appendPath(path2)
         path.stroke()
         
-        //water
+        setWaterLevel();
+    }
+    
+    func setWaterLevel(){
         UIColor.blueColor().setFill()
+        let center = CGPoint(x:bounds.width/2, y: (bounds.height/1.5))
+        let waterRadius: CGFloat = bounds.width - (bounds.width/2 - (bounds.width/3.5)) - (bounds.width/2 - (bounds.width/3.5)) - 7.5
+        let totalHeight = (((bounds.height/1.5 + 2) - bounds.height/6) + (waterRadius/2 - 2.0/2))
+        let topHeight = ((bounds.height/1.5 + 2) - bounds.height/6)
+        let bottomHeight = (waterRadius/2 - 2.0/2)
         
-        let waterRadius: CGFloat = bounds.width - (bounds.width/2 - 100) - (bounds.width/2 - 100) - 5
+        let topWaterPercentAmount = topHeight / totalHeight
+        let bottomWaterPercentAmount = bottomHeight / totalHeight
+
+        let topFilledPercent = totalHeight
+        
+        print(bottomWaterPercentAmount)
+        print(topWaterPercentAmount)
+        print((bounds.height/6) * topFilledPercent)
+        
+        //top water
+        var bottomWaterPercent: CGFloat = 0.0
+        if(currentPercentage > bottomWaterPercentAmount){
+            let path4 = UIBezierPath()
+            
+            path4.moveToPoint(CGPoint(
+            x:bounds.width/2 - (bounds.width/3.5 - 4.5),
+            y:bounds.height/6
+            ))
+            path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 - (bounds.width/3.5 - 4.5),
+            y:bounds.height/1.5 + 2
+            ))
+            path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 + (bounds.width/3.5 - 4.5),
+            y:bounds.height/1.5 + 2
+            ))
+            path4.addLineToPoint(CGPoint(
+            x:bounds.width/2 + (bounds.width/3.5 - 4.5),
+            y:bounds.height/6
+            ))
+            
+            path4.lineWidth = lineWidth
+            path4.closePath()
+            path4.fill()
+        }
+        else{
+            bottomWaterPercent = 1 - (currentPercentage / bottomWaterPercentAmount)
+        }
+
+        //bottom water
+        let calculation: CGFloat = bottomWaterPercent * 90
+        let radiansToAdd: CGFloat = calculation * CGFloat((pi/180))
+        
+        let startAngle: CGFloat = -CGFloat(CGFloat(pi) + radiansToAdd)
+        let endAngle: CGFloat = -CGFloat(0 - radiansToAdd)
         let path3 = UIBezierPath(arcCenter: center,
             radius: waterRadius/2 - 2.0/2,
             startAngle: startAngle,
             endAngle: endAngle,
-            clockwise: true)
-        path3.lineWidth = 2
+            clockwise: false)
+        path3.lineWidth = lineWidth
         path3.fill()
-        
-        let path4 = UIBezierPath()
-        path4.lineWidth = 2
-        path4.moveToPoint(CGPoint(
-            x:bounds.width/2 - 96.5,
-            y:bounds.height/6))
-        path4.addLineToPoint(CGPoint(
-            x:bounds.width/2 - 96.5,
-            y:bounds.height/1.5 + 2))
-        path4.addLineToPoint(CGPoint(
-            x:bounds.width/2 + 96.5,
-            y:bounds.height/1.5 + 2))
-        path4.addLineToPoint(CGPoint(
-            x:bounds.width/2 + 96.5,
-            y:bounds.height/6))
-        path4.closePath()
-        path4.fill()
     }
 }
